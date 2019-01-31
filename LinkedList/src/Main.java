a15882f64738fd0c0aef0b2d11c62171d77e35a9
VickyVikraman <vigramanmoorthy@gmail.com>
2019-01-31 15:18:56 +0530

    Update Main.java

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		Student s=new Student("112","Vigraman");
-		Student s1=new Student("66","Rajashri");
-		Student s2=new Student("6","Virajit");
-		Student s3=new Student("5","Rajvika");
+		Student s1=new Student("66","Sachin");
+		Student s2=new Student("6","Vignesh");
+		Student s3=new Student("5","Kiruba");
 		StudentLinkedList list=new StudentLinkedList(s);
 		list.insert(s1);
 		list.insertAtFirst(s2);
